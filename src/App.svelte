317a4a93bc27f0a9a27270a2f5d5f95106a9ae7e
J.T <tranjoh@students.zhaw.ch>
2023-05-08 14:32:14 +0200

    change let message3
    from Branch 1.2

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
 	let message = "Hello World world world this is a test for verson 1.2";
 	let message2 = "new hello world";
-	let message3 = "version 1.03";
+	let message3 = "version 1.03 changes which need to commit and push";
 </script>
 
 <div>

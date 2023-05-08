441313ee18b63b8f4a165dda6a9c5102890018e5
J.T <tranjoh@students.zhaw.ch>
2023-05-08 13:43:41 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	let message = "Hello World";
+</script>
+
+<div>
+	<h1>{message}</h1>
+</div>
+
+<style>
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
